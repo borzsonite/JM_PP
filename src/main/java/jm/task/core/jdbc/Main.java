4e8b8fef74dfdeb2f70a33a41da134910593dfe3
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-17 16:47:14 +0300

    start .save()

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,32 +1,32 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
 
-        UserDao userDaoJDBC = new UserDaoJDBCImpl();
+        UserDao userDao = new UserDaoHibernateImpl();
 
 //      1. Создание таблицы User(ов)
-        userDaoJDBC.createUsersTable();
+        userDao.createUsersTable();
 
 //      2.  Добавление 4 User(ов) в таблицу с данными на свой выбор.
 //      После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-        userDaoJDBC.saveUser("Tom", "Johns", (byte) 45);
-        userDaoJDBC.saveUser("Bob", "Smirnov", (byte) 23);
-        userDaoJDBC.saveUser("Mike", "Petrov", (byte) 34);
-        userDaoJDBC.saveUser("Maria", "Ivanova", (byte) 30);
+        userDao.saveUser("Tom", "Johns", (byte) 45);
+        userDao.saveUser("Bob", "Smirnov", (byte) 23);
+        userDao.saveUser("Mike", "Petrov", (byte) 34);
+        userDao.saveUser("Maria", "Ivanova", (byte) 30);
 
 //      3. Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
-        System.out.println(userDaoJDBC.getAllUsers());
+//        System.out.println(userDaoJDBC.getAllUsers());
 
 //      4. Очистка таблицы User(ов)
-        userDaoJDBC.cleanUsersTable();
+//        userDaoJDBC.cleanUsersTable();
 
 //      5.  Удаление таблицы
-        userDaoJDBC.dropUsersTable();
+//        userDaoJDBC.dropUsersTable();
 
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,27 +1,63 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
+
     public UserDaoHibernateImpl() {
-
     }
-
 
     @Override
     public void createUsersTable() {
-
+        try (Connection connection = Util.getJdbcConnection()) {
+            Statement statement = connection.createStatement();
+            String createTable = "CREATE TABLE IF NOT EXISTS users (\n" +
+                    "id BIGINT NOT NULL AUTO_INCREMENT,\n" +
+                    "name varchar(30),\n" +
+                    "last_name varchar(30),\n" +
+                    "age tinyint,\n" +
+                    "PRIMARY KEY (id));";
+            statement.execute(createTable);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getJdbcConnection()) {
+            Statement statement = connection.createStatement();
+            String dropTable = "DROP TABLE IF EXISTS users";
+            statement.execute(dropTable);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+
+        Transaction transaction = null;
+
+        try {
+            Session session = Util.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+        } catch(Exception e) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+        }
+
 
     }
 
