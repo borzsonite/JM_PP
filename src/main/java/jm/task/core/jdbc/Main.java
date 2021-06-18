2ebb39236d646ec6e248eded14a05582ae146f14
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-18 09:37:25 +0300

    pp_db added

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,16 +14,16 @@ public class Main {
 
 //      2.  Добавление 4 User(ов) в таблицу с данными на свой выбор.
 //      После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
-        userDaoJDBC.saveUser("Tom", "Johns", (byte) 45);
-        userDaoJDBC.saveUser("Bob", "Smirnov", (byte) 23);
-        userDaoJDBC.saveUser("Mike", "Petrov", (byte) 34);
-        userDaoJDBC.saveUser("Maria", "Ivanova", (byte) 30);
+//        userDaoJDBC.saveUser("Tom", "Johns", (byte) 45);
+//        userDaoJDBC.saveUser("Bob", "Smirnov", (byte) 23);
+//        userDaoJDBC.saveUser("Mike", "Petrov", (byte) 34);
+//        userDaoJDBC.saveUser("Maria", "Ivanova", (byte) 30);
 
 //      3. Получение всех User из базы и вывод в консоль (должен быть переопределен toString в классе User)
-        System.out.println(userDaoJDBC.getAllUsers());
+//        System.out.println(userDaoJDBC.getAllUsers());
 
 //      4. Очистка таблицы User(ов)
-        userDaoJDBC.cleanUsersTable();
+//        userDaoJDBC.cleanUsersTable();
 
 //      5.  Удаление таблицы
         userDaoJDBC.dropUsersTable();
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private static final String URL = "jdbc:mysql://localhost:3306/my_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp_db";
     private static final String USERNAME = "bestuser";
     private static final String PASSWORD = "bestuser";
     private static Connection connection = null;
